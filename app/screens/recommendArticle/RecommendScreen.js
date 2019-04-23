47298254867296f89e4f1678164f8c2783c37b36
Ivan Zhang <ivanzhang410@gmail.com>
2019-04-23 14:47:16 -0400

    fix expired navigation link

--- app/screens/recommendArticle/RecommendScreen.js
+++ app/screens/recommendArticle/RecommendScreen.js
@@ -4,52 +4,88 @@ import { RecommendArticle } from '../../components/recommendArticle';
 
 const data = [
   {
-    title: '海外看病火？为啥这么多机构都热衷投资一家海外医疗公司',
-    tag: '市场',
-    type: 1,
-    author: '第一财经',
-    img: 'https://gw.alipayobjects.com/zos/rmsportal/LwUpqblnlisckDLpBCRx.png'
+    data: {
+      title: '海外看病火？为啥这么多机构都热衷投资一家海外医疗公司',
+      tag: '市场',
+      type: 1,
+      author: '第一财经',
+      img: 'https://gw.alipayobjects.com/zos/rmsportal/LwUpqblnlisckDLpBCRx.png'
+    },
+    navigation: {
+      url: 'Details',
+      opt: {
+        title: '文章',
+        img: require("../../assets/images/culture.jpg")
+      }
+    }
   },
   {
-    title: '海外看病火？为啥这么多机构都热衷投资一家海外医疗公司',
-    tag: '市场',
-    type: 1,
-    author: '第一财经',
-    img: 'https://gw.alipayobjects.com/zos/rmsportal/beKIEIHjDxrboDvaLHZs.png'
+    data: {
+      title: '海外看病火？为啥这么多机构都热衷投资一家海外医疗公司',
+      tag: '市场',
+      type: 1,
+      author: '第一财经',
+      img: 'https://gw.alipayobjects.com/zos/rmsportal/beKIEIHjDxrboDvaLHZs.png'
+    },
+    navigation: {
+      url: 'Details',
+      opt: {
+        title: '文章',
+        img: require("../../assets/images/culture.jpg")
+      }
+    }
   },
   {
-    title: '海外看病火？为啥这么多机构都热衷投资一家海外医疗公司',
-    tag: '市场',
-    type: 1,
-    author: '第一财经',
-    img: 'https://gw.alipayobjects.com/zos/rmsportal/oIkLZVVJYahBbpztgFQv.png'
+    data: {
+      title: '海外看病火？为啥这么多机构都热衷投资一家海外医疗公司',
+      tag: '市场',
+      type: 1,
+      author: '第一财经',
+      img: 'https://gw.alipayobjects.com/zos/rmsportal/oIkLZVVJYahBbpztgFQv.png'
+    },
+    navigation: {
+      url: 'Details',
+      opt: {
+        title: '文章',
+        img: require("../../assets/images/culture.jpg")
+      }
+    }
   },
   {
-    title: '海外看病火？',
-    tag: '市场',
-    type: 1,
-    author: '第一财经',
-    img: 'https://gw.alipayobjects.com/zos/rmsportal/LwUpqblnlisckDLpBCRx.png'
+    data: {
+      title: '海外看病火？',
+      tag: '市场',
+      type: 1,
+      author: '第一财经',
+      img: 'https://gw.alipayobjects.com/zos/rmsportal/LwUpqblnlisckDLpBCRx.png'
+    },
+    navigation: {
+      url: 'Details',
+      opt: {
+        title: '文章',
+        img: require("../../assets/images/culture.jpg")
+      }
+    }
   }
 ];
 
 export default class RecommendScreen extends React.Component {
   static navigationOptions = {
     title: '热门推荐'.toUpperCase(),
-  }; 
- 
-  constructor(props){
+  };
+
+  constructor(props) {
     super(props);
   }
 
-  rendenItems(){
+  rendenItems() {
     return data.map((item, index) => {
       return (
-        <RecommendArticle 
-          key={index} 
-          data={item} 
-          handlePress = { () => {this.props.navigation.navigate("Estate")}} 
-          >
+        <RecommendArticle
+          key={index}
+          data={item.data}
+          handlePress={() => { this.props.navigation.navigate(item.navigation.url, item.navigation.opt) }}
+        >
         </RecommendArticle>
       );
     })
