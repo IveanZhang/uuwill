b193f9e0bb6ed5e96318c29b85bdf2048f13f9d1
Ivan Zhang <ivanzhang410@gmail.com>
2019-05-06 16:55:05 -0400

    add detail of product

--- app/screens/productDetailScreen/ProductDetailScreen.js
+++ app/screens/productDetailScreen/ProductDetailScreen.js
@@ -6,12 +6,9 @@ import {
     Image,
     Text
 } from 'react-native';
-import { Carousel, List } from '@ant-design/react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { Carousel, List, Tabs } from '@ant-design/react-native';
 
 const Item = List.Item;
-const Brief = Item.Brief;
 
 export default class ProductDetailScreen extends React.Component {
 
@@ -47,7 +44,90 @@ export default class ProductDetailScreen extends React.Component {
                     url: require('../../assets/images/icons/global.png'),
                     text: '英语服务'
                 }
-            ]
+            ],
+            tab: [
+                { title: '产品介绍' },
+                { title: '购前必读' },
+                { title: '退改说明' },
+            ],
+            tabContent: {
+                productProd:[
+                    {
+                        type: 'Text',
+                        style: 'highlight',
+                        text: '产品亮点',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'content',
+                        text: '1. 专业导游讲解',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'content',
+                        text: '2. 包含午餐和免费酒店接送服务',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'content',
+                        text: '3. 近距离感受大瀑布壮观之美',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'highlight',
+                        text: '4. 时间有限旅行者首选行程',
+                    },
+                    {
+                        type: 'Image',
+                        url: 'https://images.unsplash.com/photo-1550890798-715900ac1489?ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80',
+                        style: ''
+                    },
+                ],
+                beforeOrder:[
+                    {
+                        type: 'Text',
+                        style: 'title',
+                        text: '使用方法',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'content',
+                        text: '请携带电子版预定确认单（Voucher）和护照，直接前往指定地点，即可使用。',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'highlighttitle',
+                        text: '出发时间：10：00（酒店接送时间会用于出发时间30-60分钟，取决于酒店位置）',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'content',
+                        text: '酒店接送：请提前至少一天指点确认单上面活动公司的联系电话，确认具体的接送时间和地址',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'highlighttitle',
+                        text: '具体产品信息一律以Voucher（电子确认函为准）',
+                    },
+                ],
+                statement:[
+                    {
+                        type: 'Text',
+                        style: 'title',
+                        text: '年龄政策',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'content',
+                        text: '1. 13-64岁，请按照成人价格预定；65-110岁，请按老人价格预定；12岁（含）以下儿童，请按儿童价购买',
+                    },
+                    {
+                        type: 'Text',
+                        style: 'highlighttitle',
+                        text: '2. 儿童必须与至少一名成人同行，不能单独预定。',
+                    },
+                ]
+            }
         }
     }
 
@@ -71,6 +151,48 @@ export default class ProductDetailScreen extends React.Component {
                 </View>
             );
         })
+    }
+
+    renderProductProd(){
+        return this.state.tabContent.productProd.map((item, index) => {
+            return item.type === 'Text' ? 
+                item.style === 'highlight' ? 
+                    (<Text key={index} style={[styles.highlight, styles.marginTop10]}>{item.text}</Text>) : 
+                item.style === 'title' ?
+                    (<Text key={index} style={[styles.contentTitle, styles.marginTop10]}>{item.text}</Text>) :
+                    (<Text key={index} style={[styles.textGrey, styles.marginTop5]}>{item.text}</Text>)
+            :   item.type === 'Image' ?
+                    (<Image key={index} style={[styles.carousel, styles.marginTop5]} source={{url: item.url}}></Image>) :
+                    (<View></View>)
+        });
+    }
+
+    renderBeforeOrder(){
+        return this.state.tabContent.beforeOrder.map((item, index) => {
+            return item.type === 'Text' ? 
+                item.style === 'highlight' ? 
+                    (<Text key={index} style={[styles.highlight, styles.marginTop10]}>{item.text}</Text>) : 
+                item.style === 'title' ?
+                    (<Text key={index} style={[styles.contentTitle, styles.marginTop10]}>{item.text}</Text>) :
+                    (<Text key={index} style={[styles.textGrey, styles.marginTop5]}>{item.text}</Text>)
+            :   item.type === 'Image' ?
+                    (<Image key={index} style={[styles.carousel, styles.marginTop5]} source={{url: item.url}}></Image>) :
+                    (<View></View>)
+        });
+    }
+
+    renderStatement(){
+        return this.state.tabContent.statement.map((item, index) => {
+            return item.type === 'Text' ? 
+                item.style === 'highlight' ? 
+                    (<Text key={index} style={[styles.highlight, styles.marginTop10]}>{item.text}</Text>) : 
+                item.style === 'title' ?
+                    (<Text key={index} style={[styles.contentTitle, styles.marginTop10]}>{item.text}</Text>) :
+                    (<Text key={index} style={[styles.textGrey, styles.marginTop5]}>{item.text}</Text>)
+            :   item.type === 'Image' ?
+                    (<Image key={index} style={[styles.carousel, styles.marginTop5]} source={{url: item.url}}></Image>) :
+                    (<View></View>)
+        });
     }
 
     render() {
@@ -143,6 +265,22 @@ export default class ProductDetailScreen extends React.Component {
                         {this.randerFeatures()}
                     </View>
                 </View>
+
+                <View style={styles.wrapper}>
+                    <View style={styles.container}>
+                        <Tabs tabs={this.state.tab}>
+                            <View style={styles.tabs}>
+                                {this.renderProductProd()}
+                            </View>
+                            <View style={styles.tabs}>
+                                {this.renderBeforeOrder()}
+                            </View>
+                            <View style={styles.tabs}>
+                                {this.renderStatement()}
+                            </View>
+                        </Tabs>
+                    </View>
+                </View>
             </ScrollView >
         );
     }
@@ -153,6 +291,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#efefef'
     },
     wrapper: {
+        flex: 1,
         marginBottom: 15,
         backgroundColor: '#fff',
     },
@@ -177,6 +316,9 @@ const styles = StyleSheet.create({
     },
     marginLeft10: {
         marginLeft: 10
+    },
+    marginTop5: {
+        marginTop: 5
     },
     marginTop10: {
         marginTop: 10
@@ -217,5 +359,17 @@ const styles = StyleSheet.create({
         color: 'red',
         padding: 2,
         fontSize: 12
+    },
+    highlight: {
+        color: 'red',
+        padding: 2,
+        fontSize: 14
+    },
+    tabs: {
+        backgroundColor: '#fff',
+    },
+    contentTitle: {
+        fontSize: 18,
+        fontWeight: '700',
     }
 });
