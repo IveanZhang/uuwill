1468753e1305694bfe9d8fc2ae0353e0938477d0
Ivan Zhang <ivanzhang410@gmail.com>
2019-05-31 17:54:31 -0400

    add an article

--- app/data/raw/articles.js
+++ app/data/raw/articles.js
@@ -88,23 +88,18 @@ const articles = [
     },
     {
         id: 5,
-        type: 1,
-        title: 'CRO推出有望良币驱逐劣币，或进一步带动中概股价',
-        img: 'https://gw.alipayobjects.com/zos/rmsportal/beKIEIHjDxrboDvaLHZs.png',
-        tag: '文化',
-        author: '第一财经',
+        type: 2,
+        title: '嘉年华第三季度---激情夏日',
+        img: 'http://eduen.happyneg.net/wp-content/uploads/2018/12/Untitled-1-2.jpg?id=9144',
+        tag: '旅游',
+        author: '量子嘉年华',
         time: '',
         details: [
-            {type: 'subtitle', content: '2018年新增免签或相关签证政策变动回顾'},
+            {type: 'subtitle', content: '嘉年华第三季度---激情夏日'},
             {type: 'sperator', content: ''},
-            {type: 'content', content: '回顾2018这一年，我国签证的含金量可以说是一路飙升，给了人们更多的出行选择。'},
-            {type: 'content', content: '2018年1月1日，卢旺达对所有国家公民实行落地签证政策，并且中国与卢旺达互免签证政策于12月23日正式生效。'},
-            {type: 'content', content: '1月16日，中国与阿联酋互免签证。'},
-            {type: 'content', content: '1月16日，中国与阿联酋互免签证。'},
-            {type: 'content', content: '1月16日，中国与阿联酋互免签证。'},
-            {type: 'content', content: '1月16日，中国与阿联酋互免签证。'},
+            {type: 'image', height: 1300,  url: 'http://eduen.happyneg.net/wp-content/uploads/2018/12/WechatIMG29-1.jpeg'},
         ]
-    }
+    },
 ];
 
 export default articles;
